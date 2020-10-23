a7f105ebaec8f4b788d1643383ed0130f5c23226
dima <iwannaexplore@gmail.com>
2020-10-23 23:37:20 +0300

    first step

--- TicTacToe/Controllers/HomeController.cs
+++ TicTacToe/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using TicTacToe.Models;
 
 namespace TicTacToe.Controllers
 {
@@ -23,15 +17,6 @@ namespace TicTacToe.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
